4dbc20a1a9d3f674ba9bb5e3c22d9ac9809f5200
kenzie <miraclekenzie13@outlook.com>
2025-09-05 12:45:43 +0100

    my assignment

---
+++ javascript.js
@@ -0,0 +1,11 @@
+let num1 = 10;
+let num2 = 5;
+let name = 'Miracle';
+
+alert( num1 + num2 );
+alert( num1 - num2 );
+alert( num1 * num2 );
+alert( num1 / num2 );
+
+console.log( `Hello [name], the result of num1 + num2 is{result}` );
+alert( `Hello ${name}, the result of num1 + num2 is ${num1 + num2}` );
